4769d612eb107ed9f4fb32a22a400df033084338
Egor Kalashnykov <kalashnykov00@gmail.com>
2022-01-28 22:24:29 +0200

    feat: burger-menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 import * as flsFunctions from "./modules/functions.js";
 
+// Checks if browser allows to use webp format
 flsFunctions.isWebp();
+
+// Menu for mobile
+flsFunctions.menu();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,5 +1,5 @@
-export function isWebp() {
-    function testWebP(callback) {
+export const isWebp = () => {
+    const testWebP = (callback) => {
         let webP = new Image();
         webP.onload = webP.onerror = function () {
             callback(webP.height == 2);
@@ -7,9 +7,14 @@ export function isWebp() {
         webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
     }
 
-    testWebP(function (support) {
-        let className = support === true ? 'webp' : 'no-webp';
-        document.documentElement.classList.add(className);
+    testWebP((support) => {
+        document.documentElement.classList.add(support === true ? 'webp' : 'no-webp');
     });
+}
 
+export const menu = () => {
+    document.querySelector('[menu-btn]').addEventListener('click', () => {
+        document.querySelector('[menu-list]').classList.toggle('active');
+        document.querySelector('[menu-btn]').classList.toggle('active');
+    })
 }
